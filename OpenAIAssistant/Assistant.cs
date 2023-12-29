d4f4a3870d27bf980e2432547f9f7372ede4d41f
BensonTang <benson922@hotaimotor.com.tw>
2023-12-29 11:51:39 +0800

    assistant api實作3

--- OpenAIAssistant/Assistant.cs
+++ OpenAIAssistant/Assistant.cs
@@ -10,16 +10,6 @@ namespace OpenAIAssistant
     public class Assistant
     {
         private readonly IConfiguration _configuration;
-        // private readonly string file1Id = "file-9xpwQr4rni98mUcrQtlkDkvc";
-        // private readonly string instructions = @"你是GSS的請假助手，你的任務是基於上傳的檔案內容，回答使用者的問題。
-        //                                         任務說明:
-        //                                         - 當使用者提問時，仔細分析問題並基於上傳的檔案內容給出回答。
-        //                                         - 如果上傳的檔案內容沒有能回答使用者問題的參考內容，請直接回答'我不知道'";
-        // private readonly string assistantName = "GSS的請假助手";
-        // //code_interpreter, retrieval, or function
-        // private readonly object tools = new List<object> { new { type = "retrieval" } };
-        // private readonly string model = "gpt-3.5-turbo-1106";
-        // private readonly List<string> file_ids = new List<string> { file1Id };
         private static string _apiKey = "xxxxxxxxx";
 
         public Assistant(IConfiguration configuration)
--- OpenAIAssistant/Controllers/AIWeatherController.cs
+++ OpenAIAssistant/Controllers/AIWeatherController.cs
@@ -10,23 +10,30 @@ namespace OpenAIAssistant.Controllers
     [Route("api/[controller]")]
     public class AIWeatherController : ControllerBase
     {
-        private readonly IConfiguration _configuration;
-        private static string _apiKey = "xxxxxxxxx";
-        //constructor
-        public AIWeatherController(IConfiguration configuration)
+        // private readonly string file1Id = "file-9xpwQr4rni98mUcrQtlkDkvc";
+        private readonly string instructions = @"這是一個透過api取得天氣資料並回答使用者的GPT，使用者會問各縣市的天氣，
+                                                藉由呼叫GetCurrentWeather action取得縣市天氣預報，並同時呼叫GetSpecialWeather取得縣市天氣警報。
+                                                若使用者問的縣市不在台灣，請回答'您所問的地區不在偉大的台灣國喔'，不需要多餘廢話。
+                                                若使用者輸入的縣市是簡體字，請自動轉為繁體字。
+                                                若使用者沒有輸入縣市，例如只輸入臺北，請自動帶入縣市變為臺北市。
+                                                非天氣問題一律回答'老子只他媽懂天氣'";
+        private readonly string assistantName = "天氣之子";
+        //code_interpreter, retrieval, or function
+        private readonly object tools = new List<object> { new { type = "retrieval" } };
+        private readonly string model = "gpt-3.5-turbo-1106";
+        // private readonly List<string> file_ids = new List<string> { file1Id };
+        public AIWeatherController()
         {
-            _configuration = configuration;
-            _apiKey = _configuration["OpenAIServiceOptions:ApiKey"];
         }
 
         [HttpGet]
-        public async Task CreateThreadEndpoint(string city)
+        public async Task CreateThreadEndpoint()
         {
             
         }
 
         [HttpPost]
-        public async Task ChatEndpoint(string city)
+        public async Task ChatEndpoint()
         {
             
         }
