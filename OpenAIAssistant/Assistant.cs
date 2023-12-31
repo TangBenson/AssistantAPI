e747ffd619efff92dee9be1cf57ae262b3735d68
unknown <nerodmd@gmail.com>
2024-01-01 00:43:43 +0800

    20240101

--- OpenAIAssistant/Assistant.cs
+++ OpenAIAssistant/Assistant.cs
@@ -9,15 +9,15 @@ namespace OpenAIAssistant
     public class Assistant
     {
         private readonly IConfiguration _configuration;
-        private static string _apiKey = "xxxxxxxxx";
+        private readonly string? _apiKey;
 
         public Assistant(IConfiguration configuration)
         {
             _configuration = configuration;
-            _apiKey = _configuration["OpenAIServiceOptions:ApiKey"];
+            _apiKey = _configuration.GetSection("OpenAIServiceOptions")["ApiKey"].ToString();
         }
 
-        internal static async Task<string> CreateAssistant(
+        internal async Task<(string, JsonElement)> CreateAssistant(
             string instructions,
             string name,
             List<object> tools,
@@ -25,34 +25,35 @@ namespace OpenAIAssistant
             List<string> file_ids)
         {
             var url = "https://api.openai.com/v1/assistants";
-            var jsonStr = JsonSerializer.Serialize(new
-            {
-                instructions,
-                name,
-                tools,
-                model,
-                file_ids
-            });
+            var jsonStr = JsonSerializer.Serialize(
+                new
+                {
+                    model,
+                    name,
+                    instructions,
+                    tools,
+                    // file_ids
+                });
             var responseRoot = await CallAPI(url, jsonStr, "POST");
             return responseRoot.ValueKind == JsonValueKind.Undefined ?
-                    "" :
-                    responseRoot.GetProperty("id").GetString()!;
+                    ("", default) :
+                    (responseRoot.GetProperty("id").GetString()!, responseRoot);
         }
 
         //要查看有那些 Assistants 可以呼叫 ListAssistants()
-        internal static async Task<string> ListAssistants()
+        internal async Task<string> ListAssistants()
         {
             var url = "https://api.openai.com/v1/assistants?order=desc&limit=20";
 
             var responseRoot = await CallAPI(url, null, "GET");
-            return(
+            return (
                 responseRoot.ValueKind == JsonValueKind.Undefined ?
                 "" :
                 responseRoot.GetProperty("data").ToString()!);
         }
 
         // 建立 Thread
-        internal static async Task<string> CreateThread()
+        internal async Task<string> CreateThread()
         {
             var url = "https://api.openai.com/v1/threads";
 
@@ -63,7 +64,7 @@ namespace OpenAIAssistant
         }
 
         //發送使用者 Message
-        internal static async Task CreateMessage(string threadId, string question)
+        internal async Task CreateMessage(string threadId, string question)
         {
             Console.WriteLine($"發送msg給LLM");
             var url = $"https://api.openai.com/v1/threads/{threadId}/messages";
@@ -81,7 +82,7 @@ namespace OpenAIAssistant
         }
 
         // 取回 Message
-        internal static async Task ListMessages(string threadId)
+        internal async Task ListMessages(string threadId)
         {
             Console.WriteLine($"取得LLM回傳的msg");
             var url = $"https://api.openai.com/v1/threads/{threadId}/messages?order=desc&limit=2";
@@ -95,7 +96,7 @@ namespace OpenAIAssistant
         }
 
         //發送 Run 來讓 Assistant 處理使用者詢問的問題
-        internal static async Task CreateRun(string threadId, string assistantId)
+        internal async Task CreateRun(string threadId, string assistantId)
         {
             Console.WriteLine($"執行Run");
             var url = $"https://api.openai.com/v1/threads/{threadId}/runs";
@@ -132,7 +133,7 @@ namespace OpenAIAssistant
             }
         }
 
-        internal static async Task<string> RetrieveRun(string threadId, string runId)
+        internal async Task<string> RetrieveRun(string threadId, string runId)
         {
             var url = $"https://api.openai.com/v1/threads/{threadId}/runs/{runId}";
             var responseRoot = await CallAPI(url, null, "get");
@@ -140,7 +141,7 @@ namespace OpenAIAssistant
             return status;
         }
 
-        internal static async Task ListRunSteps(string threadId, string runId)
+        internal async Task ListRunSteps(string threadId, string runId)
         {
             var url = $"https://api.openai.com/v1/threads/{threadId}/runs/{runId}/steps";
             var responseRoot = await CallAPI(url, null, "get");
@@ -151,7 +152,7 @@ namespace OpenAIAssistant
         }
 
         //將 Thread 刪除掉
-        internal static async Task DeleteThread(string threadId)
+        internal async Task DeleteThread(string threadId)
         {
             var url = $"https://api.openai.com/v1/threads/{threadId}";
 
@@ -177,7 +178,7 @@ namespace OpenAIAssistant
             }
         }
 
-        private static async Task<JsonElement> CallAPI(
+        private async Task<JsonElement> CallAPI(
             string url,
             string jsonStr,
             string method)
@@ -186,20 +187,16 @@ namespace OpenAIAssistant
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {_apiKey}");
             client.DefaultRequestHeaders.Add("OpenAI-Beta", "assistants=v1");
 
-            using var jsonContent = new StringContent(jsonStr,
-                Encoding.UTF8,
-                "application/json");
-
-            var response = method == "POST" ? 
-                await client.PostAsync(url, jsonContent) : 
+            var response = method == "POST" ?
+                await client.PostAsync(url, new StringContent(jsonStr,Encoding.UTF8,"application/json")) :
                 await client.GetAsync(url);
-            Console.WriteLine(response);
+            Console.WriteLine($"FYI ~ {response.Content.ReadAsStringAsync().Result}");
             if (response.IsSuccessStatusCode)
             {
                 //從HTTP響應中讀取內容並轉換為字符串
                 var responseBody = await response.Content.ReadAsStringAsync();
                 //將JSON字符串解析為 JsonDocument
-                using JsonDocument doc = JsonDocument.Parse(responseBody);
+                JsonDocument doc = JsonDocument.Parse(responseBody);
                 //獲取解析後的JSON文檔的根元素
                 JsonElement root = doc.RootElement;
                 return root;
@@ -208,6 +205,26 @@ namespace OpenAIAssistant
             {
                 Console.WriteLine($"OpenAI API回傳錯誤:{response.StatusCode}-{response.ReasonPhrase}");
                 return default;
+            }
+        }
+        internal static string GetRumor(string personName)
+        {
+            switch (personName)
+            {
+                case "周杰倫":
+                    return "周杰倫的緋聞對象是昆凌";
+                case "昆凌":
+                    return "昆凌的緋聞對象是周杰倫";
+                case "蔡依林":
+                    return "蔡依林的緋聞對象是周杰倫";
+                case "周渝民":
+                    return "周渝民的緋聞對象是阿尼亞";
+                case "川普":
+                    return "川普的緋聞對象是....數不完";
+                case "唐瑋祁":
+                    return "他太神祕了，沒有緋聞對象";
+                default:
+                    return "這難倒我了，我不知道";
             }
         }
     }
--- OpenAIAssistant/Controllers/AIWeatherController.cs
+++ OpenAIAssistant/Controllers/AIWeatherController.cs
@@ -11,41 +11,39 @@ namespace OpenAIAssistant.Controllers
     public class AIWeatherController : ControllerBase
     {
         // private readonly string file1Id = "file-9xpwQr4rni98mUcrQtlkDkvc";
-        private readonly string instructions = @"這是一個透過api取得天氣資料並回答使用者的GPT，使用者會問各縣市的天氣，
-                                                藉由呼叫GetCurrentWeather action取得縣市天氣預報，並同時呼叫GetSpecialWeather取得縣市天氣警報。
-                                                若使用者問的縣市不在台灣，請回答'您所問的地區不在偉大的台灣國喔'，不需要多餘廢話。
-                                                若使用者輸入的縣市是簡體字，請自動轉為繁體字。
-                                                若使用者沒有輸入縣市，例如只輸入臺北，請自動帶入縣市變為臺北市。
-                                                非天氣問題一律回答'老子只他媽懂天氣'";
-        private readonly string assistantName = "天氣之子";
-        //code_interpreter, retrieval, or function
-        private readonly List<object> tools = new() {
+        private readonly string instructions = "我會告訴你最新演藝圈八卦";
+        private readonly string assistantName = "狗仔隊";
+        private readonly List<object> tools = new()
+        {
             new { type = "retrieval" },
             new { type = "code_interpreter" },
             new {
                 type = "function",
                 function = new {
-                    name= "get_city_for_date",
-                    description= "根据传入的日期获取对应的城市.",
+                    name = "GetRumor",
+                    description = "根據傳入的人名，取得他的緋聞對象名字",
                     parameters = new {
-                        type="object",
-                        properties=new {
-                            date_str=new {
-                                type= "string",
-                                description= "用于获取对应城市的日期，格式为YYYY-MM-DD."
+                        type = "object",
+                        properties = new {
+                            personName = new {
+                                type = "string",
+                                description = "取得人名"
                             }
-                        }
+                        },
+                        required = new List<string> { "personName" }
                     }
                 }
             }
         };
-        private readonly string model = "gpt-3.5-turbo-1106";
+        private readonly string model = "gpt-4-1106-preview";
+        // private readonly string model = "gpt-3.5-turbo-1106";
+        // private static string _assistantId = "";//gpt-4-preview-1106
+        private static string _assistantId = "asst_yRXtzWuhFOPv1xqZrcHj5rrV";//gpt-3.5-turbo-1106
         // private readonly List<string> file_ids = new List<string> { file1Id };
-        // private readonly Assistant _assistant;
-        private static string _assistantId = "asst_yRXtzWuhFOPv1xqZrcHj5rrV";
-        public AIWeatherController()
+        private readonly Assistant _assistant;
+        public AIWeatherController(Assistant assistant)
         {
-            // _assistant = assistant;
+            _assistant = assistant;
         }
 
         // 這樣會404，why?
@@ -54,37 +52,46 @@ namespace OpenAIAssistant.Controllers
         //     _assistantId == "" ? await Assistant.CreateAssistant(instructions, assistantName, tools, model, null) : "";
 
         // 已經取過了就不用再取了
-        // [HttpGet]
-        // public async Task GetAssistant()
-        // {
-        //     if (_assistantId == "")
-        //     {
-        //         _assistantId = await Assistant.CreateAssistant(instructions, assistantName, tools, model, new List<string>());
-        //         Console.WriteLine($"建立assistantId成功");
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine($"已經有assistantId了");
-        //     }
-        // }
+        [HttpGet]
+        public async Task<string> GetAssistant()
+        {
+            if (_assistantId == "")
+            {
+                (_assistantId, var jsonStr) = await _assistant.CreateAssistant(
+                    instructions,
+                    assistantName,
+                    tools,
+                    model,
+                    new List<string>());
+                return @$"建立assistantId成功 - {_assistantId}
+                .
+                .
+                .
+                {jsonStr}";
+            }
+            else
+            {
+                return $"已經有assistantId了";
+            }
+        }
 
         [HttpGet]
         public async Task<ActionResult> GetAssistantsList() =>
-            Ok(await Assistant.ListAssistants());
+            Ok(await _assistant.ListAssistants());
 
         //前端要存著threadId
         [HttpGet]
         public async Task<ActionResult> CreateThreadEndpoint() =>
-            Ok(await Assistant.CreateThread());
+            Ok(await _assistant.CreateThread());
 
         [HttpPost]
         public async Task<(string rmsg, string img)> ChatEndpoint(
             string msg,
             string threadId)
         {
-            await Assistant.CreateMessage(msg, threadId);
-            await Assistant.CreateRun(threadId, _assistantId);
-            await Assistant.ListMessages(threadId);
+            await _assistant.CreateMessage(msg, threadId);
+            await _assistant.CreateRun(threadId, _assistantId);
+            await _assistant.ListMessages(threadId);
             return ("", "");
         }
     }
